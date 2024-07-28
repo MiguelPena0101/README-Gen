3bf81e505f8935eeb29a79bc3e79ffc47a81fb5c
Rey <Miguel.Pena1709@gmail.com>
2024-07-28 19:04:40 -0400

    add packages & arrays

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,56 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+    },
+    {
+    type: 'input',
+    name: 'description',
+    message: 'Please write a short description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What are the installation instructions?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide instructions and examples for usage:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide guidelines for contributing:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide examples of how to run tests:',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
